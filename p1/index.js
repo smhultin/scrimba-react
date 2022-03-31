33f95d5a46ba69504d7d7652358a73bd7a0d934a
Sarah <smhultin@rams.colostate.edu>
2022-03-30 20:30:36 -0700

    Updated content from 'lesson'

--- p1/index.js
+++ p1/index.js
@@ -1,4 +1,24 @@
+// React is composable and declarative
+// JSX = Java Script Xml
+
+function MainContent () {
+    return (
+        <p>I'm learning React!</p>
+    )
+}
+
+const navbar = (
+  <nav>
+    <h1>Sarah :)</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  navbar,
   document.getElementById("root")
 )
